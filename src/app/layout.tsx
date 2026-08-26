3676056fa8c377cfba9ffb2be5f388b1d4dc672a
meganwu <meganbxwu@gmail.com>
2026-08-25 21:46:58 -0400

    meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
 import { cn } from "@/utils/cn";
 
 const inter = FontSans({
@@ -9,14 +10,24 @@ const inter = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "Daily Digest",
-  description: "A day at a time of your Gmail.",
+  title: "digest",
+  description: "a daily digest of your gmail.",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={cn(inter.variable, "antialiased")}>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <title>digest</title>
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/public/mailbox_with_mail.png"
+        ></link>
+      </head>
+      <body className="min-h-full flex flex-col">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
